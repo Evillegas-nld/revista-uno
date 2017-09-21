734cd384cd289ae46aa63965775982388d0a11ba
Evillegas-nld <evillegas.nld@gmail.com>
2017-09-21 18:53:42 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,423 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>This P/N, a branding studio</title>
+    <link rel="stylesheet" href="css/app.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
+    <link href="css/fontello.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="css/font-awesome.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="css/hero.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="css/slick.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="css/slick-theme.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="css/animate.css" media="screen" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="css/superslides.css" />
+    <script src="js/modernizr.js"></script>
+  </head>
+  <body>
+    <div class='contain-to-grid sticky transparent'>
+      <div class='title-area'>
+        <div class="logo-nav">
+          <a href='index.html'>
+            P<span>/</span>N
+          </a>
+          <nav class='small-nav' data-options='sticky_on: large' data-topbar=''>
+            <ul>
+              <li class='active'>
+                <a href='index.html'>Home</a>
+    <!--             <ul class="sub-menu">
+                  <li><a href="index.html">Home default</a></li>
+                  <li><a href="index.html">Home alternative</a></li>
+                </ul> -->
+              </li>
+              <li class=''>
+                <a href='about.html'>About</a>
+              </li>
+              <li class=''>
+                <a href='services.html'>Services</a>
+              </li>
+              <li class=''>
+                <a href='works.html'>Works</a>
+              </li>
+              <li class=''>
+                <a href='blog.html'>Blog</a>
+              </li>
+              <li class=''>
+                <a href='contact.html'>Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+
+    <nav class='big-nav' data-options='sticky_on: large' data-topbar=''>
+      <ul>
+        <li class='active  wow slideInLeft' data-wow-delay="0s">
+          <a href='index.html'>Home</a>
+<!--           <ul class="sub-menu">
+            <li><a href="index.html">Home default</a></li>
+            <li><a href="index.html">Home alternative</a></li>
+          </ul> -->
+        </li>
+        <li class=' wow slideInLeft' data-wow-delay="0.1s">
+          <a href='about.html'>About</a>
+        </li>
+        <li class=' wow slideInLeft' data-wow-delay="0.2s">
+          <a href='services.html'>Services</a>
+        </li>
+        <li class=' wow slideInLeft' data-wow-delay="0.3s">
+          <a href='works.html'>Works</a>
+        </li>
+        <li class=' wow slideInLeft' data-wow-delay="0.4s">
+          <a href='blog.html'>Blog</a>
+        </li>
+        <li class=' wow slideInLeft' data-wow-delay="0.5s">
+          <a href='contact.html'>Contact</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="full no-padding">
+      <div id="slides">
+        <div class="slides-container">
+          <div class="slide">
+            <div class="title-wrapper">
+              <div class="inner">
+                <h1>Better <span>branding</span> experience</h1>
+              </div>
+            </div>
+            <div class="color-overlay red"></div>
+            <img src="img/slide-7.jpg" alt="Cinelli">
+          </div>
+          <div class="slide">
+            <div class="title-wrapper">
+              <div class="inner">
+                <h1>Infinit <span>designing</span> Solutions</h1>
+              </div>
+            </div>
+            <div class="color-overlay yellow"></div>
+            <img src="img/slide-2.jpg" alt="Cinelli">
+          </div>
+          <div class="slide">
+            <div class="title-wrapper">
+              <div class="inner">
+                <h1>The most <span>efficient</span>workflow</h1>
+              </div>
+            </div>
+            <div class="color-overlay cyan"></div>
+            <img src="img/slide-3.jpg" alt="Cinelli">
+          </div>
+        </div>
+        <nav class="slides-navigation">
+          <a href="#" class="next">Next</a>
+          <a href="#" class="prev">Previous</a>
+        </nav>
+      </div>
+    </div>
+    <div class="full white centered-text bg-1">
+      <div class="row">
+        <div class="large-10 large-centered columns">
+          <h2 class='title wow fadeInUp' >Is there a more creative and efficient way to build a brand? <span>We think so</span>.</h2>
+          <div class="intro wow fadeInUp" data-wow-delay="0.3s">
+            <p>Welcome to PN. This is a startup company crafting brands, including print work, web design, packgaing work and so on. We have a group of telented designers who will give you better design experience.</p>
+          </div>
+          <a href="about.html" class="button small boxed black wow fadeInUp" data-wow-delay="0.5s">Learn More</a>
+          <div class="three spacing"></div>
+        </div>
+      </div>
+
+    </div>
+    <div class="full">
+      <div class="row">
+        <div class="large-12 columns">
+          <h2 class="section-title">
+            / recent works
+          </h2>
+        </div>
+      </div>
+      <div class="works">
+        <div class="row">
+          <div class="large-3 columns">
+            <div class="work wow rollIn right" data-wow-delay="0.6s">
+              <a href="work-single.html">
+                <img src="img/work-1.jpg" alt="">
+                <div class="info">
+                  <h3>Project name</h3>
+                  <p>Branding, print</p>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="large-3 columns">
+            <div class="work wow rollIn right" data-wow-delay="0.3s">
+              <a href="work-single.html">
+                <img src="img/work-2.jpg" alt="">
+                <div class="info">
+                  <h3>Project name</h3>
+                  <p>Branding, print</p>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="large-3 columns">
+            <div class="work wow rollIn right" data-wow-delay="0.1s">
+              <a href="work-single.html">
+                <img src="img/work-3.jpg" alt="">
+                <div class="info">
+                  <h3>Project name</h3>
+                  <p>Branding, print</p>
+                </div>
+              </a>
+            </div>
+          </div><div class="large-3 columns">
+            <div class="work wow rollIn right" data-wow-delay="">
+              <a href="work-single.html">
+                <img src="img/work-4.jpg" alt="">
+                <div class="info">
+                  <h3>Project name</h3>
+                  <p>Branding, print</p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="large-3 columns">
+            <div class="work wow rollIn" data-wow-delay="0.7s">
+              <a href="work-single.html">
+                <img src="img/work-5.jpg" alt="">
+                <div class="info">
+                  <h3>Project name</h3>
+                  <p>Branding, print</p>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="large-3 columns">
+            <div class="work wow rollIn" data-wow-delay="0.4s">
+              <a href="work-single.html">
+                <img src="img/work-6.jpg" alt="">
+                <div class="info">
+                  <h3>Project name</h3>
+                  <p>Branding, print</p>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="large-3 columns">
+            <div class="work wow rollIn" data-wow-delay="0.2s">
+              <a href="work-single.html">
+                <img src="img/work-7.jpg" alt="">
+                <div class="info">
+                  <h3>Project name</h3>
+                  <p>Branding, print</p>
+                </div>
+              </a>
+            </div>
+          </div><div class="large-3 columns">
+            <div class="work wow rollIn" data-wow-delay="0.1s">
+              <a href="work-single.html">
+                <img src="img/work-8.jpg" alt="">
+                <div class="info">
+                  <h3>Project name</h3>
+                  <p>Branding, print</p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+      <div class="two spacing"></div>
+    </div>
+    <div class="full no-padding dark light">
+      <div class="services">
+        <div class="color-overlay red fadeinleft"></div>
+        <div class="row">
+          <div class="large-5 columns">
+            <div class="row">
+              <div class="large-9 columns">
+                <h2 class="section-title white">/ main services</h2>
+                <div class="intro white">
+                  <p>The aim of marketing is to know and understand the customer so well the product or service fits him and sells itself.</p>
+                </div>
+                <a href="services.html" class="button small boxed white">Learn More</a>
+                <div class="three spacing"></div>
+              </div>
+              <div class="large-3 columns"></div>
+            </div>
+          </div>
+          <div class="large-7 columns">
+            <div class="service-items">
+              <div class="service fadeinright" data-delay="100">
+                <i class="fa fa-flag-o"></i>
+                <h3 class='white'>Branding</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, reiciendis.</p>
+              </div>
+              <div class="service fadeinright" data-delay="200">
+                <i class="fa fa-laptop"></i>
+                <h3 class='white'>Web design</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, reiciendis.</p>
+              </div>
+              <div class="service fadeinright" data-delay="300">
+                <i class="fa fa-gift"></i>
+                <h3 class='white'>Packaging</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, reiciendis.</p>
+              </div>
+              <div class="service fadeinright" data-delay="400">
+                <i class="fa fa-inbox"></i>
+                <h3 class='white'>Print work</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, reiciendis.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="full parallax bg-2">
+      <div class="row">
+        <div class="large-12 columns">
+          <h2 class="section-title">
+            / Testimonials
+          </h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="large-6 columns">
+          <div class='item moveup' data-delay='100'>
+            <div class="quote">
+              <p><i class='fa fa-quote-left'></i>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat consectetuer sit amet magna adipiscing."</p>
+            </div>
+            <div class='author'>
+              <p class='author-avatar'>
+                <img width="80" height="80" alt="" src="img/author-avatar-1.png" />
+              </p>
+              <p class='author-name'>
+                <strong>
+                  Lily Moss
+                </strong>
+              </p>
+              <p>
+               Designer at Huglab
+             </p>
+           </div>
+         </div>
+          <div class='item moveup' data-delay='300'>
+            <div class="quote">
+              <p><i class='fa fa-quote-left'></i>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat consectetuer sit amet magna adipiscing."</p>
+            </div>
+            <div class='author'>
+              <p class='author-avatar'>
+                <img width="80" height="80" alt="" src="img/author-avatar-3.png" />
+              </p>
+              <p class='author-name'>
+                <strong>
+                  Lily Moss
+                </strong>
+              </p>
+              <p>
+               Designer at Huglab
+             </p>
+           </div>
+         </div>
+       </div>
+       <div class="large-6 columns">
+        <div class='item moveup' data-delay='200'>
+          <div class='author'>
+            <p class='author-avatar'>
+              <img width="80" height="80" alt="" src="img/author-avatar-2.png" />
+            </p>
+            <p class='author-name'>
+
+              <strong>
+                David White
+              </strong>
+            </p>
+            <p> Designer at Huglab</p>
+          </div>
+          <div class="quote reverse">
+            <p><i class='fa fa-quote-left'></i>Magni repellendus non aliquam, ut culpa fuga necessitatibus ea laboriosam praesentium, minima, unde accusantium quaerat vel."</p>
+          </div>
+        </div>
+        <div class='item moveup' data-delay='400'>
+          <div class='author'>
+            <p class='author-avatar'>
+              <img width="80" height="80" alt="" src="img/author-avatar-1.png" />
+            </p>
+            <p class='author-name'>
+
+              <strong>
+                David White
+              </strong>
+            </p>
+            <p> Designer at Huglab</p>
+          </div>
+          <div class="quote reverse">
+            <p><i class='fa fa-quote-left'></i>Repellat saepe facere necessitatibus soluta, perspiciatis aperiam non et qui eaque tenetur neque quidem excepturi ipsum odit ducimus, temporibus nemo unde blanditiis."</p>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class="full no-padding">
+      <div class='images'>
+        <div class="large-4 small-6 columns">
+          <div><a href="#"><img alt="" src="img/image-1.jpg" /></a></div>
+        </div>
+        <div class="large-4 small-6 columns">
+          <div><a href="#"><img alt="" src="img/image-2.jpg" /></a></div>
+        </div>
+        <div class="large-4 small-6 columns">
+          <div><a href="#"><img alt="" src="img/image-3.jpg" /></a></div>
+        </div>
+        <div class="large-4 small-6 columns">
+          <div><a href="#"><img alt="" src="img/image-4.jpg" /></a></div>
+        </div>
+        <div class="large-4 small-6 columns">
+          <div><a href="#"><img alt="" src="img/image-5.jpg" /></a></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="full dark no-padding">
+      <div class="footer">
+        <div class="row">
+          <div class="large-6 columns">
+            <ul class="socials">
+              <li><a href="#"><i class='fa fa-twitter'></i></a></li>
+              <li><a href="#"><i class='fa fa-facebook'></i></a></li>
+              <li><a href="#"><i class='fa fa-linkedin'></i></a></li>
+              <li><a href="#"><i class='fa fa-dribbble'></i></a></li>
+            </ul>
+          </div>
+          <div class="large-6 columns">
+            <ul>
+              <li>41 Dean Street, London, UK.</li>
+              <li>+86 (03) 9810 4612</li>
+              <li><a href="#">info@Tato-team.com</a></li>
+            </ul>
+            <p>©2015 P/N. All rights reserved.</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script src="js/jquery.superslides.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/app.js"></script>
+    <script src="js/jquery.appear.js"></script>
+    <script src="js/slick.min.js"></script>
+  </body>
+</html>
